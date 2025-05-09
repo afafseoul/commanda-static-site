ed5762efbf128aab1e3f6996e753afbb77f9cec1
adrien khireche <adrienkhireche@MacBook-Pro-de-adrien-143.local>
2025-05-09 16:54:11 +0900

    swag

--- script.js
+++ script.js
@@ -22,3 +22,24 @@ if (signupForm) {
     }
   });
 }
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+
+const supabase = createClient(
+  'https://jgdsbsgajoidkqiwndnp.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnZHNic2dham9pZGtxaXduZG5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUwOTA4MTMsImV4cCI6MjAyMDY2NjgxM30.y25cXK8kuOlLDnbcrAnwXQ2UhhOpV3NuIXkNrrRZ5g'
+);
+
+// LOGIN GOOGLE – 🔥 C’EST CE QUI MANQUE !
+const googleLoginBtn = document.getElementById("google-login");
+if (googleLoginBtn) {
+  googleLoginBtn.addEventListener("click", async () => {
+    console.log("Bouton Google cliqué ✅");
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: 'https://cozy-maamoul-92d86f.netlify.app/dashboard.html'
+      }
+    });
+    if (error) alert("Erreur OAuth : " + error.message);
+  });
+}
